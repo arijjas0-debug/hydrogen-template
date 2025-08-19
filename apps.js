1ca49652cf6f6e08946060f0669932b04964731d
arijjas0-debug <arijjas0@gmail.com>
2025-08-19 10:15:59 +0700

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,5 @@
+import { tambah, pi } from './math.js';
+
+let hasil = tambah(5, 3);
+console.log("Hasil: " + hasil);
+console.log("Nilai pi: " + pi);
---
+++ math.js
@@ -0,0 +1,6 @@
+
+export function tambah(a, b) {
+  return a + b;
+}
+
+export const pi = 3.14159;
